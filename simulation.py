1ef45d86e971ce4ced32002d27e7c911dfc1549a
betato <battlestargatticus@gmail.com>
2022-12-19 12:31:00 -0500

    Add douglas-peucker data decimation

--- simulation.py
+++ simulation.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from math import log10, floor
+from math import log10, floor, sqrt
 
 t = 0.0
 TIMESTEP = 0.001
@@ -108,20 +108,55 @@ def _scale_plots(plot: PlotData):
     plt.xlim(x_lims)
     plt.ylim(y_lims)
 
+def _douglas_peucker(xs: list[float], ys: list[float], epsilon: float):
+    dmax = 0
+    imax = 0
+    for i in range(2, len(xs)):
+        x1 = xs[0]
+        x2 = xs[-1]
+        y1 = ys[0]
+        y2 = ys[-1]
+        dx = x2-x1
+        dy = y2-y1
+        d = abs((y1-ys[i])*dx - (x1-xs[i])*dy) / sqrt(dx**2 + dy**2)
+        if (d > dmax):
+            imax = i
+            dmax = d
+
+    if (dmax > epsilon):
+        xs_1, ys_1 = _douglas_peucker(xs[:imax], ys[:imax], epsilon)
+        xs_2, ys_2 = _douglas_peucker(xs[imax:], ys[imax:], epsilon)
+        return ((xs_1+xs_2), (ys_1+ys_2))
+    else:
+        return ([xs[0], xs[-1]], [ys[0], ys[-1]])
+
+def _decimate(data: PlotData.Series):
+    delta = max(max(data.ys)-min(data.xs), max(data.xs)-min(data.xs))
+    epsilon = delta/100000
+    old_len = len(data.xs)
+    data.xs, data.ys = _douglas_peucker(data.xs, data.ys, epsilon)
+    print('old:{}, new:{}, ({}%)'.format(old_len, len(data.xs), round(100*len(data.xs)/old_len, 4)))
+
+
 def draw_plots():
     for plot in _plot_vec.values():
         plt.title(plot.title)
         plt.gcf().canvas.manager.set_window_title(plot.title)
+
+        if plot.annotate_max >= 0:
+            for series in plot.data.values():
+                _an_max(series, plot)
+
+        for series in plot.data.values():
+            _decimate(series)
+
+        for series in plot.data.values():
+            _plot_series(series)
+
         if plot.xlabel != '' or plot.xunits != '':
             plt.xlabel('{} ({})'.format(plot.xlabel, plot.xunits))
         if plot.ylabel != '' or plot.yunits != '':
             plt.ylabel('{} ({})'.format(plot.ylabel, plot.yunits))
-
-        for series in plot.data.values():
-            _plot_series(series)
-            if plot.annotate_max >= 0:
-                _an_max(series, plot)
-
         if len(plot.data) > 1:
             plt.legend(loc="upper right")
 
